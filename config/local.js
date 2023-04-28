2bd63afa4b5c069f50639ed1e772d12c361ddb3f
Johnny Hausman <johnny@hausmanfamily.com>
2023-04-28 02:00:43 -0500

    [wip] refactor config/local.js

--- config/local.js
+++ config/local.js
@@ -1,18 +1,8 @@
 /*
  * log_manager
  */
-function env(envKey, defaultValue) {
-   if (typeof process.env[envKey] == "undefined") {
-      return defaultValue;
-   }
-   try {
-      return JSON.parse(process.env[envKey]);
-   } catch (e) {
-      console.log(e);
-      console.log(process.env[envKey]);
-      return process.env[envKey];
-   }
-}
+const AB = require("@digiserve/ab-utils");
+const env = AB.defaults.env;
 
 module.exports = {
    log_manager: {
@@ -21,33 +11,16 @@ module.exports = {
       /*************************************************************************/
       enable: env("LOG_MANAGER_ENABLE", true),
 
-      sentry: {
+      sentry: env("SENTRY_CONFIG", {
          dns: env("SENTRY_DNS", "sentry.url.here"),
-         sampleRate: env("SENTRY_SAMPLE_RATE", 1.0),
+         tracesSampleRate: env("SENTRY_SAMPLE_RATE", 1.0),
          serverName: env("SENTRY_SERVER_NAME", "AppBuilder"),
-      },
+      }),
    },
 
    /**
     * datastores:
     * Sails style DB connection settings
     */
-   datastores: {
-      appbuilder: {
-         adapter: "sails-mysql",
-         host: env("MYSQL_HOST", "db"),
-         port: env("MYSQL_PORT", 3306),
-         user: env("MYSQL_USER", "root"),
-         password: process.env.MYSQL_PASSWORD,
-         database: env("MYSQL_DBPREFIX", "appbuilder"),
-      },
-      site: {
-         adapter: "sails-mysql",
-         host: env("MYSQL_HOST", "db"),
-         port: env("MYSQL_PORT", 3306),
-         user: env("MYSQL_USER", "root"),
-         password: process.env.MYSQL_PASSWORD,
-         database: env("MYSQL_DBADMIN", "appbuilder-admin"),
-      },
-   },
+   datastores: AB.defaults.datastores(),
 };
