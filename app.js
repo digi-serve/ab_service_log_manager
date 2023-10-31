01fb691337ed3f5a19724f2c3f5047e4bc938b97
Nate <7259@pm.me>
2023-10-31 14:39:25 +0700

    init sentry by default

--- app.js
+++ app.js
@@ -3,11 +3,14 @@
 // (AppBuilder) A log manager for various AB operations
 //
 const AB = require("@digiserve/ab-utils");
-
-if(process.env.SENTRY_DSN){
-   const { version } = require("./package");
-   AB.telemetry().init({
-      dsn: process.env.SENTRY_DSN,
+const { version } = require("./package");
+// Use sentry by default, but can override with env.TELEMETRY_PROVIDER
+if (AB.defaults.env("TELEMETRY_PROVIDER", "sentry") == "sentry") {
+   AB.telemetry.init("sentry", {
+      dsn: AB.defaults.env(
+         "SENTRY_DSN",
+         "https://2c6d39a4a232e87591840bcf3d8ca948@o144358.ingest.sentry.io/4505945305120768"
+   ),
       release: version,
    });
 }
@@ -16,3 +19,4 @@ const controller = AB.controller("log_manager");
 // controller.afterStartup((cb)=>{ return cb(/* err */) });
 // controller.beforeShutdown((cb)=>{ return cb(/* err */) });
 controller.init();
+
